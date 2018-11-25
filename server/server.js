ea96947b1a9630f871d46d6a5af359225360b487
Creek Gem <creekgem@outlook.com>
2018-11-25 14:51:25 +0500

    Few events added

--- server/server.js
+++ server/server.js
@@ -1,13 +1,41 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
 const port = process.env.PORT || 3000;
-
 const publicPath = path.join(__dirname, '../public')
+
+const server = http.createServer(app)
+let io = socketio(server)
+
 app.use(express.static(publicPath))
 
+io.on('connection', (socket)=>{
 
-app.listen(port, ()=>{
+    console.log('New user connected');
+    
+    socket.on('disconnect', ()=>{
+        console.log('Client disconnect!');
+        
+    })
+
+    socket.on('createMessage', (message)=>{
+        console.log('createMessage', message);
+        
+    })
+
+    socket.emit('newMessage', {
+        from: 'Waseem',
+        text: 'what\'s up',
+        createdAt: 154565
+    })
+
+})
+
+
+
+server.listen(port, ()=>{
     console.log(`Server started at :${port}`);
 })
